aa98203ec98fbd122ec4a5644dd3afd55b9eb974
moAtef <mohamedatef65@gmail.com>
2023-02-15 20:08:36 +0200

    created html and css files and basec js commands

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// get all elements from HTML:
+var uEmail = document.getElementById("email");
+var uName = document.getElementById("name");
+var uPassword = document.getElementById("password");
+var userInc = document.getElementById("alertUserIncorrect");
+var userExits = document.getElementById("alertUserExits");
+var passwordInc = document.getElementById("alertPasswordIncorrect");
+var loginBtn = document.getElementById("loginBtn");
+var regBtn = document.getElementById("regBtn");
+var signUp = document.getElementById("signUpNow");
+
+//chaeck all elements:
+// console.log(uEmail,uName,uPassword,userInc,userExits,passwordInc,loginBtn,regBtn,signUp);
+
